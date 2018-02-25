6948c91e46608f6a47a4e1cd2415fcdbf505bb4a
Derek Edwards <dedwards8@gmail.com>
2018-02-25 14:24:35 -0500

    Add recent activity to the home page

---
+++ app/controllers/admin/cleanings_controller.rb
@@ -0,0 +1,17 @@
+class Admin::CleaningsController < ApplicationController
+
+  def create
+    @cleaning = Cleaning.new
+    @cleaning.update_attributes(cleaning_params)
+    @cleaning.time = Time.now
+    @cleaning.save
+    redirect_to admin_block_path @cleaning.block
+  end 
+
+  private
+
+  def cleaning_params
+    params.require(:cleaning).permit(:note, :block_id)
+  end
+
+end
