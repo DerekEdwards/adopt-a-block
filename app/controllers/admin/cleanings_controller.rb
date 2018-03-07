84b2a69b9b24ae7a367e1da36ed294376e94a4d0
Derek Edwards <dedwards8@gmail.com>
2018-03-06 22:26:45 -0500

    You can edit cleanings Fixes #15 (#22)
    * You can edit cleanings.
    
    * You can edit cleanings.

--- app/controllers/admin/cleanings_controller.rb
+++ app/controllers/admin/cleanings_controller.rb
@@ -8,10 +8,29 @@ class Admin::CleaningsController < ApplicationController
     redirect_to admin_block_path @cleaning.block
   end 
 
+  def edit
+    @cleaning = Cleaning.find(params[:id])
+    @block = @cleaning.block
+  end
+
+  def update
+    @cleaning = Cleaning.find(params[:id])
+    @cleaning.update_attributes!(cleaning_params)
+    #@cleaning.time = Time.now
+    @cleaning.save!
+    redirect_to admin_block_path @cleaning.block
+  end
+
+  def destroy
+    @cleaning = Cleaning.find(params[:id])
+    @cleaning.destroy 
+    redirect_to admin_block_path @cleaning.block
+  end
+
   private
 
   def cleaning_params
-    params.require(:cleaning).permit(:note, :block_id, :before_photo, :after_photo)
+    params.require(:cleaning).permit(:note, :time, :block_id, :before_photo, :after_photo)
   end
 
 end
