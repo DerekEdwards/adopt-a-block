58f4c5124ec9a41fb8f7026a440013b4a72f9ce0
Derek Edwards <dedwards8@gmail.com>
2018-05-21 21:48:05 -0400

    Another Temp

--- app/controllers/admin/blocks_controller.rb
+++ app/controllers/admin/blocks_controller.rb
@@ -6,8 +6,10 @@ class Admin::BlocksController < ApplicationController
   end 
 
   def create
+    puts params[:polyline]
     @block = Block.new(block_params)
     @block.neighborhood = Neighborhood.find(params[:neighborhood_id])
+    @block.polyline = converted_polyline
     @block.save
     redirect_to admin_neighborhood_path @block.neighborhood
   end
@@ -21,6 +23,14 @@ class Admin::BlocksController < ApplicationController
 
   def block_params
     params.require(:block).permit(:name, :description, :polyline)
+  end
+
+  def converted_polyline
+    formatted_polyline = []
+    block_params["polyline"].gsub('(','').gsub(')','').gsub(' ','').split(',').in_groups_of(2) do |latlng|
+      formatted_polyline << {lat: latlng.first.to_f, lng: latlng.last.to_f}
+    end
+    formatted_polyline
   end
 
 end
