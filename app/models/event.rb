a7f8dd645dedc4afb0f733ba1c41119e3f310136
Derek Edwards <dedwards8@gmail.com>
2019-11-26 20:53:51 -0500

    Show event on neighborhood page.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,11 @@ class Event < ApplicationRecord
   belongs_to :user
   belongs_to :neighborhood
 
-   scope :future, ->{ where('event_date >= ?', Time.now) }
-   scope :past, ->{ where('event_date < ?', Time.now) }
+  scope :future, ->{ where('event_date >= ?', Time.now) }
+  scope :past, ->{ where('event_date < ?', Time.now) }
+
+  def duration
+    self.start_time.strftime("%I:%M %p") + " to " + self.end_time.strftime("%I:%M %p")
+  end
 
 end
