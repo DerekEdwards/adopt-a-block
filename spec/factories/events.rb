06106eb0356d1fd18ab71d0191ba639492ff6084
Derek Edwards <dedwards8@gmail.com>
2020-03-29 15:23:35 -0400

    #96 Add event tests.

---
+++ spec/factories/events.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :event do
+    name { "1 Hour Clean-Up" }
+    description { "Quick Clean Up" }
+    location_description { "Meet at the flag pole." }
+    event_date { "12/12/2050".to_date }
+    start_time { "12/12/2050 10AM".to_time }
+    end_time { "12/12/2050 11AM".to_time }
+  end
+end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:my_neighborhood) { FactoryBot.create :neighborhood }
+  let(:user) { FactoryBot.create :user }
+  let(:my_event) { FactoryBot.create(:event, neighborhood: my_neighborhood, user: user) }
+
+  it { should respond_to :name }
+  it { should respond_to :description }
+  it { should respond_to :location_description }
+  it { should respond_to :event_date}
+  it { should respond_to :start_time}
+  it { should respond_to :end_time}
+  it { should respond_to :user}
+  it { should respond_to :neighborhood }
+  it { should respond_to :canceled}
+
+  it 'knows its duration' do
+    expect(my_event.duration).to eq("10:00 AM to 11:00 AM")
+  end
+
 end
