adf2b58485e7f148d892aba692c289f6f3782e86
Derek Edwards <dedwards8@gmail.com>
2020-03-29 16:09:33 -0400

    Update test #96

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     description { "Quick Clean Up" }
     location_description { "Meet at the flag pole." }
     event_date { "12/12/2050".to_date }
-    start_time { "12/12/2050 10AM".to_time }
-    end_time { "12/12/2050 11AM".to_time }
+    start_time { "12/12/2050 10AM".to_time.in_time_zone }
+    end_time { "12/12/2050 11AM".to_time.in_time_zone }
   end
 end
