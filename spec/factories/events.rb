890294af79110d72465e24c10464eb400286c4c0
Derek Edwards <dedwards8@gmail.com>
2020-03-29 16:17:44 -0400

    #96 Last chance at fixing the Travis timezone issue.

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     description { "Quick Clean Up" }
     location_description { "Meet at the flag pole." }
     event_date { "12/12/2050".to_date }
-    start_time { "12/12/2050 10AM".to_time.in_time_zone }
-    end_time { "12/12/2050 11AM".to_time.in_time_zone }
+    start_time { Time.zone.local(2050, 12, 12, 10, 00)}
+    end_time { Time.zone.local(2050, 12, 12, 11, 00) }
   end
 end
