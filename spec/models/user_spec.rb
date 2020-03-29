c3e0705e0488cc87a7de8793eedb38ba5d47b2aa
Derek Edwards <dedwards8@gmail.com>
2020-03-29 16:42:19 -0400

    Add tests for user #96

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:my_neighborhood) { FactoryBot.create :neighborhood }
+  let(:user) { FactoryBot.create :user }
+  
+  it { should respond_to :admin }
+  it { should respond_to :blocks }
+  it { should respond_to :events }
+  it { should respond_to :neighborhoods }
+
+  it 'follows a neighborhood' do
+    expect(user.neighborhoods.count).to eq 0
+    user.follow my_neighborhood
+    expect(user.neighborhoods.count).to eq 1
+  end
+
+  it 'unfollows a neighborhood' do
+    expect(user.neighborhoods.count).to eq 0
+    user.follow my_neighborhood
+    expect(user.neighborhoods.count).to eq 1
+    user.unfollow my_neighborhood
+    expect(user.neighborhoods.count).to eq 0
+  end
+
+  it 'checks to see if it follows a neighborhood' do
+    expect(user.follows?(my_neighborhood)).to eq false
+    user.follow my_neighborhood
+    expect(user.follows?(my_neighborhood)).to eq true
+  end
+
 end
