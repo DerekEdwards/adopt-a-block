ffc775e09e0cafbf9df10ad5753a72e0cea8d1c6
Derek Edwards <dedwards8@gmail.com>
2020-03-29 15:41:20 -0400

    #96 add tests for configs.

---
+++ spec/factories/configs.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :config do
+    key { "favorite_color" }
+    value { "blue" }
+  end
+end
--- spec/models/config_spec.rb
+++ spec/models/config_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Config, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:color_config) { FactoryBot.create :config}
+
+  it { should respond_to :key }
+  it { should respond_to :value }
+
+  it 'handles a missing config' do 
+    expect(Config.bad_one).to eq nil
+  end
+
+  it 'handles a missing config' do 
+    expect(Config.favorite_color).to eq "blue"
+  end
+
+  it 'creates a new config' do
+    Config.new_one = "new one"
+    expect(Config.new_one).to eq "new one"
+  end
+
+  it 'updates a config' do
+    expect(Config.favorite_color).to eq "blue"
+    Config.favorite_color = "green"
+    expect(Config.favorite_color).to eq "green"
+  end
+
 end
