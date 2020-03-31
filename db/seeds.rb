3e4a7e6ed3abbbef70371501be0a199a6460fc5f
Derek Edwards <dedwards8@gmail.com>
2020-03-31 19:23:22 -0400

    Add seeds to create an admin user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#<User id: 1, email: "dedwards8@gmail.com", created_at: "2018-02-26 01:47:00", updated_at: "2020-03-28 16:37:40", name: "Derek Edwards", admin: true, subscribed_to_reminders: true, subscribed_to_neighborhood_updates: true>
+User.create!(
+    email: 'admin@example.com', 
+    name: "Default User (Delete Me", 
+    admin: true,
+    password: "Welcome1",
+    password_confirmation: "Welcome1")
